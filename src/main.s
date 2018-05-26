2ceebbe1107f19dbe0a0eaf01bd3662fc98a9642
Gareth Nelson <gareth@garethnelson.com>
2018-05-26 23:21:55 +0100

    basic mario spritesheet

--- src/main.s
+++ src/main.s
@@ -5,24 +5,26 @@
 Main:
 	OAM_init shadow_oam, 0, 0, 0
 
-	load_spriteset pikachu_tiles, pikachu_palette ; load pikachu
+	load_spriteset mario_sprites_tiles, mario_sprites_palette ; load mario_sprites
 	load_bg bg01_map, bg01_tiles, bg01_palette    ; load bg01
 
 	setup_screen
-
 
 	; set Vblank handler
 	VBL_set VBL
 
 	screen_on
 
+
+
 loop:
+
 	jmp loop
 
 VBL:
-	; set a single sprite (ID 0) at 100,100 with tile ID set to 0
-	; with 32x32 sprites, it's important to remember to add 4, not 1 when animating
-	sprite_update 0, 100, 100, 0
+
+	sprite_update 0, 10,  40, #10
+	sprite_update 1, 230, 40, #0
 
 
 
@@ -33,7 +35,6 @@ VBL:
 
 .segment "LORAM"
 shadow_oam: .res 512+32
-pika_frame: .res 2
 
 .segment "RODATA"
 ; the background image
@@ -41,6 +42,6 @@ incbin bg01_palette, "data/bg01.png.palette"
 incbin bg01_tiles,   "data/bg01.png.tiles.lz4"
 incbin bg01_map,     "data/bg01.png.map.lz4"
 
-; pikachu animation
-incbin pikachu_tiles,      "data/pikachu.png.tiles.lz4"
-incbin pikachu_palette,    "data/pikachu.png.palette"
+; mario_sprites animation
+incbin mario_sprites_tiles,      "data/mario_sprites.png.tiles.lz4"
+incbin mario_sprites_palette,    "data/mario_sprites.png.palette"
